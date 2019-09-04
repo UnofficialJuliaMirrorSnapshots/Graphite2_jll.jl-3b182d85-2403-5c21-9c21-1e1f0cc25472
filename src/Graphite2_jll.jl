91eeff03c9b0c90752d64644fdb92f6c2f89875f
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-04 11:51:49 +0000

    Snapshot of branch master taken on 2019-09-04T07:51:48.233-04:00 from "https://github.com/UnofficialJuliaMirror/Graphite2_jll.jl-3b182d85-2403-5c21-9c21-1e1f0cc25472"

---
+++ src/Graphite2_jll.jl
@@ -0,0 +1,26 @@
+module Graphite2_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("3b182d85-2403-5c21-9c21-1e1f0cc25472"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Graphite2", artifacts_toml) for e in artifacts["Graphite2"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Graphite2; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Graphite2_jll
